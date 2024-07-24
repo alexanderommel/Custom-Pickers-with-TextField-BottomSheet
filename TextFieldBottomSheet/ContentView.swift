47a09baf4a46b20a71532d22b2154e0bc9628704
Rommel <alexanderommelsw@outlook.es>
2024-07-24 01:07:12 -0500

    app

---
+++ TextFieldBottomSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TextFieldBottomSheet
+//
+//  Created by Rommel Valdiviezo on 23/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TextFieldBottomSheet/TextFieldBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldBottomSheetApp.swift
+//  TextFieldBottomSheet
+//
+//  Created by Rommel Valdiviezo on 23/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
